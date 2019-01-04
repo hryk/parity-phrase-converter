96de2502d1cd872ace34b2283b8d01dcedd3a4bb
1VQ9 <hi@1vq9.com>
2019-01-05 00:48:27 +0900

    Initial commit.

---
+++ README.md
@@ -0,0 +1,16 @@
+Parity Phrase Converter
+=======================
+
+Convert Parity Phrase to Web3 secret storage JSON file.
+
+### Usage
+
+```
+$ npx parity_phrase_converter
+Type parity word phrase:
+Address: 0x0000000000000000000000000000000000000000
+Type password to create web3 secret storage: *************
+Re-type password: *************
+Keystore is generated at: keystore.json
+```
+
---
+++ src/index.js
@@ -0,0 +1,66 @@
+const fs = require('fs');
+const {Command, flags} = require('@oclif/command');
+const parityWordlist = require('@parity/wordlist');
+const ethUtil = require('ethereumjs-util');
+const Wallet = require('ethereumjs-wallet');
+const hdkey = require('ethereumjs-wallet/hdkey');
+const {cli} = require('cli-ux');
+
+function fromParityPhrase(phrase) {
+    let hash = ethUtil.keccak(Buffer.from(phrase), 256);
+    for (let i = 0; i < 16384; i++) {
+      hash = ethUtil.keccak(hash, 256);
+    }
+    while (ethUtil.privateToAddress(hash)[0] != 0) {
+      hash = ethUtil.keccak(hash, 256);
+    }
+    return hash
+}
+
+class ParityPhraseConverterCommand extends Command {
+    async run() {
+        const {flags} = this.parse(ParityPhraseConverterCommand);
+        const phrase = await cli.prompt("Type parity word phrase");
+        if (parityWordlist.verifyPhrase(phrase)) {
+            const privkey_buff = fromParityPhrase(phrase);
+            const restored_wallet = Wallet.fromPrivateKey(privkey_buff);
+            this.log("Address: " + restored_wallet.getChecksumAddressString());
+            // Export wallet as Web3 secret storage
+            //  https://github.com/ethereum/wiki/wiki/Web3-Secret-Storage-Definition
+            let password_confirmation = false;
+            let confirmed_password = null;
+            let num_try = 1;
+            while (!password_confirmation || num_try >= 3) {
+                const password = await cli.prompt("Type password to create web3 secret storage", {type: 'hide'});
+                const password_confirm = await cli.prompt("Re-type password", {type: 'hide'});
+                if (password === password_confirm) {
+                    password_confirmation = true;
+                    confirmed_password = password;
+                }
+                num_try += 1
+            }
+            if (password_confirmation === true && confirmed_password !== null) {
+                const keystore = restored_wallet.toV3(confirmed_password);
+                fs.writeFile(flags.output, JSON.stringify(keystore), (err) => {
+                    if (err) throw err;
+                    this.log("Keystore is generated at: " + flags.output);
+                });
+            }
+            else {
+                this.log("Password confirmation failed.");
+            }
+        }
+        else {
+            this.log("Invalid phrase. Plese double check your input.");
+        }
+    }
+}
+
+ParityPhraseConverterCommand.description = "Parity phrase converter converts parity phrase to bip32 mnemonic phrase.";
+ParityPhraseConverterCommand.flags = {
+    "version": flags.version({"char": "v"}),
+    "help": flags.help({"char": "h"}),
+    "output": flags.string({"char": "o", "default": "keystore.json"})
+};
+
+module.exports = ParityPhraseConverterCommand;
